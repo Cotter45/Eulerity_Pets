4ab6b31abe685217b2d383797e97334b184d9e6e
Sean <sd.cotter45@gmail.com>
2021-11-08 21:11:57 -0500

    moved the fdispatch to app instead of home to load a bit earlier and account for refreshes

--- src/components/main/rescues/pet_profile.js
+++ src/components/main/rescues/pet_profile.js
@@ -1,5 +1,12 @@
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function PetProfile() {
+    const { name } = useParams();
+
+    const pets = useSelector(state => state.Pets_Data.pets);
+
+    // const pet = pets?.find(pet => /pet.name/i.match(name));
 
     return (
         <div>
